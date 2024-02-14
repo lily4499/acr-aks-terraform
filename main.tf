68c9e81717b57ec6ece8bf7035fdaae93c3ecc9d
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-14 18:49:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "null_resource" "create_storage" {
       az group create --name $RESOURCE_GROUP_NAME --location eastus
 
       # Create storage account
-      az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+      az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services lily
 
       # Create blob container
       az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
