780b830d6cc8867ddb037beb8a0f607b5ddc2791
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-14 18:04:03 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,8 +11,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id       = "689aee19-d95c-4dfb-9b53-7a103ded4190"
-  tenant_id             = "98932122-35ff-47e5-a539-0942b55eb83c"
-  client_id             = "ae3623f2-05e8-4b53-ba3c-1e4e98a5433a"
-  client_secret         = "mpY8Q~22DXTl~XF1Q6lC_QeF33PshfljmXGrlayZ"
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+ 
 }
