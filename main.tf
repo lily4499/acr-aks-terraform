79c1c52295c61f4327e2bb7f1fd960803433bc93
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-14 12:03:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,9 +28,46 @@ output "username" {
   value = azurerm_container_registry.acr.admin_username
 }
 
-# Output Password
-output "password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
+# After ACR is created, you will get admin password from access keys in your repository, then it will be use in jenkins credentials.
+
+
+# Create AKS Cluster and Grant ACR Pull acess
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "lili_cluster"                  #your AKS name
+  location            = azurerm_resource_group.arg.location
+  resource_group_name = azurerm_resource_group.arg.name
+  dns_prefix          = "liliaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_A2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
 }
 
+# Add authentication command for AKS
+resource "null_resource" "authenticate_aks" {
+  provisioner "local-exec" {
+    command = <<EOT
+      az aks get-credentials --resource-group ${azurerm_resource_group.arg.name} --name ${azurerm_kubernetes_cluster.aks.name} --overwrite-existing
+    EOT
+
+    interpreter = ["bash", "-c"]
+  }
+}
+
+# Create acr pull access for AKS Cluster
+resource "azurerm_role_assignment" "arm_role" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
