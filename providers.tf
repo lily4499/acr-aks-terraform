d291c70e223cba86ac2dd626969a733a8216862d
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-14 18:34:05 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -14,6 +14,6 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
-  client_secret   = var.client_secret
+  client_secret   = var.client_secret_base64
  
 }
