f06df52df83d26777f93f5268b24aa7b19286003
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-15 15:52:17 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,7 +5,13 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
-}
+  }
+backend "azurerm" {
+    resource_group_name   = "tfstaterg"  # Specify the Azure resource group for backend storage
+    storage_account_name  = "lilibackendsa"  # Specify the name of the Azure Storage Account
+    container_name        = "tfstatect"               # Specify the container within the Storage Account
+    key                   = "terraform.tfstate"      # Specify the filename for the Terraform state file
+  }
 }
 
 # Configure the Microsoft Azure Provider
