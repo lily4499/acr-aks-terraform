8750bc5bff5e3a99dd060a5c67ef9b0fb8534fc2
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-15 16:03:10 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -10,6 +10,7 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
